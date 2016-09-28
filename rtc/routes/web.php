14cd1ff55ff448a83d8e35df633d8a4b46c80e9b
Wator Vapor <watorvapor@gmail.com>
2016-09-29 00:13:57 +0900

    Update web.php

--- rtc/routes/web.php
+++ rtc/routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 */
 
 Route::group(['prefix' => 'rtc'], function () {
-    Route::get('/', function () {
+    Route::get('/welcome', function () {
         return view('welcome');
     });
 });
