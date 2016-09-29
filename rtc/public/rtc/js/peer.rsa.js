79c27d67fc68a2584596fde3c57d50379b4a1b33
Wator Vapor <watorvapor@gmail.com>
2016-09-29 13:19:03 +0900

    signal key

--- rtc/public/rtc/js/peer.rsa.js
+++ rtc/public/rtc/js/peer.rsa.js
@@ -1,9 +1,43 @@
 var PeerRSA = PeerRSA || {};
+PeerRSA.uri = PeerRSA.uri || 'wss://' + location.host + '/rtc/wss';
 /*
   PeerRSA.A is Peer create RSA key.
 */
-PeerRSA.A = PeerRSA.A || {};
-PeerRSA.A.createKey = function createKey() {
+PeerRSA.A = function () {
+  this.wss = PeerRSA.A.wss || new WebSocket(PeerRSA.uri,'wator.rtc.a');
+  this.wss.onopen = function (event) {
+    this.signalOpened(event);
+  }
+  this.wss.onclose = function (event) {
+    this.signalClosed(event);
+  }
+  this.wss.onerror = function (err) {
+    console.error(err);
+  }
+  this.wss.onmessage = function (event) {
+    this.onMsg_(event).bind(this);
+  }
+}
+
+PeerRSA.A.prototype.signalOpened = function (event) {
+  console.log(event);
+}
+PeerRSA.A.prototype.signalClosed = function (event) {
+  console.log(event);
+}
+PeerRSA.A.prototype.onMsg_ = function (event) {
+  console.log(event);
+  console.log(this);
+}
+PeerRSA.A.prototype.signal_ = function (msg) {
+  console.log(event);
+  console.log(this);
+  var tokenA = localStorage.getItem('rtc.PeerRSA.A.token');
+  var sigMsg = {token:tokenA,rtc:msg};
+  this.wss.send(JSON.string(sigMsg));
+}
+
+PeerRSA.A.prototype.createKey = function () {
   //console.log(KEYUTIL.version);
   var rsaKeypair = KEYUTIL.generateKeypair("RSA", 4096);
   //console.log(rsaKeypair);
@@ -15,19 +49,33 @@ PeerRSA.A.createKey = function createKey() {
   //console.log(pubKey);
   var pubKeyStr = KEYUTIL.getPEM(pubKey);
   localStorage.setItem('rtc.PeerRSA.A.publicKey',pubKeyStr);
-  var token = KJUR.crypto.Util.sha512(pubKeyStr);
+  var token = KJUR.crypto.Util.sha1(pubKeyStr);
   //console.log(token);
   localStorage.setItem('rtc.PeerRSA.A.token',token);
 }
 
+
+/*
+ config : {A:{video:{},audio:{}},B:{video:{},audio:{}}}
+*/
+PeerRSA.A.prototype.connect = function (config) {
+  console.log(this);
+  var msg = {cmd:'start',body:config};
+  this->signal_(msg);
+}
+
+
 /*
   PeerRSA.B is Peer import RSA key.
 */
-PeerRSA.B = PeerRSA.B || {};
+PeerRSA.B = function () {
+  PeerRSA.B.wss = PeerRSA.B.wss || new WebSocket(PeerRSA.uri,'wator.rtc.b');
+}
 PeerRSA.B.importKey = function (pubKey) {
   //
-  var token = KJUR.crypto.Util.sha512(pubKey);
+  var token = KJUR.crypto.Util.sha1(pubKey);
   var storageKey = 'rtc.PeerRSA.B.token.' + token;
   localStorage.setItem(storageKey,pubKey);
 }
+
 
