85c7e79594e073240ca4604e9104372899e48ccf
Wator Vapor <watorvapor@gmail.com>
2016-09-29 14:57:49 +0900

    Update peer.rsa.js

--- rtc/public/rtc/js/peer.rsa.js
+++ rtc/public/rtc/js/peer.rsa.js
@@ -5,17 +5,18 @@ PeerRSA.uri = PeerRSA.uri || 'wss://' + location.host + '/rtc/wss';
 */
 PeerRSA.A = function () {
   this.wss = PeerRSA.A.wss || new WebSocket(PeerRSA.uri,'wator.rtc.a');
+  var self = this;
   this.wss.onopen = function (event) {
-    this.signalOpened(event);
+    self.signalOpened(event);
   }
   this.wss.onclose = function (event) {
-    this.signalClosed(event);
+    self.signalClosed(event);
   }
   this.wss.onerror = function (err) {
     console.error(err);
   }
   this.wss.onmessage = function (event) {
-    this.onMsg_(event).bind(this);
+    self.onSignalMsg_(event).bind(this);
   }
 }
 
@@ -57,7 +58,28 @@ PeerRSA.A.prototype.connect = function (config) {
 */
 PeerRSA.B = function () {
   this.wss = this.wss || new WebSocket(PeerRSA.uri,'wator.rtc.b');
+  var self = this;
+  this.wss.onopen = function (event) {
+    self.signalOpened(event);
+  }
+  this.wss.onclose = function (event) {
+    self.signalClosed(event);
+  }
+  this.wss.onerror = function (err) {
+    console.error(err);
+  }
+  this.wss.onmessage = function (event) {
+    self.onSignalMsg_(event).bind(this);
+  }
 }
+
+PeerRSA.B.prototype.signalOpened = function (event) {
+  console.log(event);
+}
+PeerRSA.B.prototype.signalClosed = function (event) {
+  console.log(event);
+}
+
 PeerRSA.B.prototype.importKey = function (newPubKey) {
   //
   try {
@@ -101,6 +123,22 @@ PeerRSA.A.prototype.sendSignal_ = function (msg) {
   };
   this.wss.send(JSON.stringify(wsMsg));
 }
+
+PeerRSA.B.prototype.onSignalMsg_ = function (event) {
+  console.log(event);
+  console.log(this);
+}
+PeerRSA.B.prototype.sendSignal_ = function (msg,token) {
+  console.log(event);
+  console.log(this);
+  var wsMsg = {
+    token:token,
+    rtc:msg
+  };
+  this.wss.send(JSON.stringify(wsMsg));
+}
+
+
 PeerRSA.signature_ = function(orig) {
   try {
     var privateKey = localStorage.getItem('rtc.PeerRSA.A.privateKey');
