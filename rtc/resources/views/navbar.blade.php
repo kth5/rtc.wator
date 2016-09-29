bf0ff413f3646b4c65dba0cc9cf6b5e75cc6d3ca
Wator Vapor <watorvapor@gmail.com>
2016-09-29 23:43:20 +0900

    navbar

---
+++ rtc/resources/views/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="//www.watorvapor.com/rtc/keys">
+        <img alt="Rsa Key" src="//www.watorvapor.com/rtc/img/keys.jpg">
+      </a>
+      <a class="navbar-brand" href="//www.watorvapor.com/rtc/cast">
+        <img alt="Live Cast" src="//www.watorvapor.com/rtc/img/cast.jpg">
+      </a>
+      <a class="navbar-brand" href="//www.watorvapor.com/rtc/catch">
+        <img alt="Catch" src="//www.watorvapor.com/rtc/img/catch.jpg">
+      </a>
+    </div>
+  </div>
+</nav>
