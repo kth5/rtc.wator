71578caa85887925f13d4b459734d5eec0decaf3
Wator Vapor <watorvapor@gmail.com>
2016-10-05 22:31:07 +0900

    Update cast.blade.php

--- rtc/resources/views/cast.blade.php
+++ rtc/resources/views/cast.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container bg-warning container-connection">
 <div class="row">
-<button type="button" class="btn btn-info btn-lg btn-block" id="btn-wait">Wait</button>
+<button type="button" class="btn btn-info btn-lg btn-block" id="btn-wait">Start wait connection from remote devices.</button>
 </div>
 </div>
 <script type="text/javascript">
