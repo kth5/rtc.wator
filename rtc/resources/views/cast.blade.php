577e6656c91902e3e39eea3672fb20ad4865d6f7
Wator Vapor <watorvapor@gmail.com>
2016-10-05 22:25:45 +0900

    Update cast.blade.php

--- rtc/resources/views/cast.blade.php
+++ rtc/resources/views/cast.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 @section('content')
-<div class="container bg-warning container-mime-key">
+<div class="container bg-warning container-connection">
 <div class="row">
 <button type="button" class="btn btn-info btn-lg btn-block" id="btn-wait">Wait</button>
 </div>
