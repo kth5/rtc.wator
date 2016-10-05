c7dae8f6e160745e8607cce8bcd204e56e7fa9e1
Wator Vapor <watorvapor@gmail.com>
2016-10-05 21:36:40 +0900

    Update web.php

--- rtc/routes/web.php
+++ rtc/routes/web.php
@@ -10,11 +10,19 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-Route::group(['prefix' => 'rtc'], function () {
-    Route::get('/top', 'TopController@index');
-    Route::get('/keys', 'KeysController@index');
-    Route::get('/cast', 'CastController@index');
-    Route::get('/catch', 'CatchController@index');
-});
-Route::get('/', 'TopController@index');
 
+
+/*
+Route::group(['prefix' => 'rtc'], function () {
+  Route::get('/top', 'TopController@index');
+  Route::get('/keys', 'KeysController@index');
+  Route::get('/cast', 'CastController@index');
+  Route::get('/catch', 'CatchController@index');
+});
+*/
+
+Route::get('/', 'TopController@index');
+Route::get('/top', 'TopController@index');
+Route::get('/keys', 'KeysController@index');
+Route::get('/cast', 'CastController@index');
+Route::get('/catch', 'CatchController@index');
