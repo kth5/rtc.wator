7913d0df09d76a2b77881590a46d866a4fdc903b
Wator Vapor <watorvapor@gmail.com>
2016-10-06 22:49:54 +0900

    Update catch.blade.php

--- rtc/resources/views/catch.blade.php
+++ rtc/resources/views/catch.blade.php
@@ -5,12 +5,21 @@
   <button type="button" class="btn btn-info btn-lg btn-block" id="btn-connect">Connect</button>
 </div>
 </div>
+<div class="container bg-info container-video">
+<div class="row">
+   <video id="remoteVideo" width="640" height="480" autoplay></video>
+</div>
+</div>
+
 <script type="text/javascript">
 $(document).ready(function() {
   var peerA = new PeerRSA.A();
     $('#btn-connect').click(function(){
       peerA.connect({B:{video:true,audio:true}});
     });
+    peerA.onaddstream = function(src) {
+      $('#remoteVideo').attr('src', src);
+    };
 });
 </script>
 @endsection
