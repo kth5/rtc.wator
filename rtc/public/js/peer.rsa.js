3e0e790cf0a986af4cc98181978b03f76bbc41ca
Wator Vapor <watorvapor@gmail.com>
2016-10-08 21:47:28 +0900

    Update peer.rsa.js

--- rtc/public/js/peer.rsa.js
+++ rtc/public/js/peer.rsa.js
@@ -224,7 +224,7 @@ PeerRSA.A.prototype.onAddIceCandidateFailure_ = function(e) {
 
 PeerRSA.A.prototype.onSetRemoteDescriptionSuccess_ = function() {
   console.log('onSetRemoteDescriptionSuccess_');
-  this.mediaConst = {'mandatory': {'OfferToReceiveVideo': true, 'OfferToReceiveAudio':true}};
+  this.mediaConst = {'mandatory': {'OfferToReceiveVideo': true}};
   console.log(this.mediaConst);
   this.catch_.pc.createAnswer(this.onCreateAnswerSuccess_.bind(this),this.onCreateAnswerError_.bind(this),this.mediaConst); 
 }
