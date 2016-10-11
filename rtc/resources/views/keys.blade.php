1c152b8528f0c0a1ea1ce4934f493ba3c6883fe2
Wator Vapor <watorvapor@gmail.com>
2016-10-11 23:39:13 +0900

    Update keys.blade.php

--- rtc/resources/views/keys.blade.php
+++ rtc/resources/views/keys.blade.php
@@ -8,7 +8,7 @@
       <div class="row">
         <button type="button" class="btn btn-danger btn-sm" id="btn-create-key">recreate</button>
         <button type="button" class="btn btn-info btn-sm  pull-right btn-clipboard" data-clipboard-target="#text-this-device-key">Copy</button>
-        <pre id="text-this-device-key" class="text-danger" rows="20"></pre>
+        <pre id="text-this-device-key" class="text-danger small" rows="20"></pre>
         <h5 class="text-info">Please send this public key to your another device,by e-mail,sns or etc.</h5>
       </div>
     </div>
