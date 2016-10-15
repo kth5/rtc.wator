4099b2ad6d71a2933b139c7ad6f09495c43b7dc8
Wator Vapor <watorvapor@gmail.com>
2016-10-15 23:47:49 +0900

    Update cast.blade.php

--- rtc/resources/views/cast.blade.php
+++ rtc/resources/views/cast.blade.php
@@ -3,7 +3,7 @@
 <div class="container bg-warning container-connection">
   <div class="row">
     <div id="device-camera" class="col-md-6"></div>
-    <div id="device-audio" class="col-md-6"></div>
+    <div id="device-mic" class="col-md-6"></div>
   </div>
   <div class="row">
     <button type="button" class="btn btn-success btn-lg btn-block" id="btn-standby">Standby for remote devices.</button>
@@ -17,18 +17,25 @@
 $(document).ready(function() {
   PeerRSA.gatherDevice(function(device){
     console.log(device);
-    var inner = '<label class="radio-inline"><input type="radio" name="';
+    var inner = '<label class="radio-inline"><input type="radio" '
+    if(device.kind === 'videoinput') {
+      inner += 'name="device-camera"';
+    }
+    if(device.kind === 'audioinput') {
+      inner += 'name="device-mic"';
+    }
+    inner += 'value="';
     inner += device.deviceId;
     inner += '">';
     inner += device.label;
     inner += '</label>';
     if(device.kind === 'videoinput') {
       $("#device-camera").append(inner);
-      $("#device-audio").append(inner);
+      $("#device-mic").append(inner);
     }
     if(device.kind === 'audioinput') {
       $("#device-camera").append(inner);
-      $("#device-audio").append(inner);
+      $("#device-mic").append(inner);
     }
   })
   var peer;
