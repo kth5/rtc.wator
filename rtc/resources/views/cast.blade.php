6dc4f4a7d16bb04e04cb68d57462ab6252d9968d
Wator Vapor <watorvapor@gmail.com>
2016-10-15 23:36:59 +0900

    Update cast.blade.php

--- rtc/resources/views/cast.blade.php
+++ rtc/resources/views/cast.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <div class="container bg-warning container-connection">
   <div class="row">
-    <div class="col-md-6"></div>
-    <div class="col-md-6"></div>
+    <div id="device-camera" class="col-md-6"></div>
+    <div id="device-audio" class="col-md-6"></div>
   </div>
   <div class="row">
     <button type="button" class="btn btn-success btn-lg btn-block" id="btn-standby">Standby for remote devices.</button>
@@ -17,6 +17,17 @@
 $(document).ready(function() {
   PeerRSA.gatherDevice(function(device){
     console.log(device);
+    var inner = '<label class="radio-inline"><input type="radio" name="';
+    inner += device.deviceId;
+    inner += '">';
+    inner += device.label;
+    inner += '</label>';
+    if(device.kind === 'videoinput') {
+      $("#device-camera").append(inner);
+    }
+    if(device.kind === 'audioinput') {
+      $("#device-audio").append(inner);
+    }
   })
   var peer;
   $('#btn-standby').click(function() {
